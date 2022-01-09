b3eb3efa693eb3b7d7d52b7d62ce14632a6bffa0
christsaragih <christian@labster.com>
2022-01-09 21:50:45 +0700

    Asset bundle added

---
+++ Assets/BundleWebLoader.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using System;
+
+public class BundleWebLoader : MonoBehaviour
+{
+    public string bundleUrl ;
+    public string assetName ;
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        using (WWW web = new WWW(bundleUrl))
+        {
+            yield return web;
+            AssetBundle remoteAssetBundle = web.assetBundle;
+            if (remoteAssetBundle == null)
+            {
+                Debug.LogError(remoteAssetBundle.GetAllAssetNames());
+                yield break;
+            }
+              Instantiate(remoteAssetBundle.LoadAsset(assetName));
+            remoteAssetBundle.Unload(false);
+        }
+    }
+
+
+}
